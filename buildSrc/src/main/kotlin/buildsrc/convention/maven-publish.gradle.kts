9e5cac526449df557a7c17391ae20cba4cf845b3
Adam <897017+aSemy@users.noreply.github.com>
2022-04-03 23:19:08 +0200

    fix maven publishing

--- buildSrc/src/main/kotlin/buildsrc/convention/maven-publish.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/maven-publish.gradle.kts
@@ -4,18 +4,21 @@ plugins {
   `maven-publish`
 }
 
-plugins.withType(JavaPlugin::class.java) {
-  publishing {
-    publications {
-      create<MavenPublication>("mavenJava") {
-        from(components["java"])
-      }
-    }
-  }
-}
+//plugins.withType(JavaPlugin::class.java) {
+//  publishing {
+//    publications {
+//      create<MavenPublication>("mavenJava") {
+//        from(components["java"])
+//      }
+//    }
+//  }
+//}
 
 tasks
-  .matching { it.name in listOf("publish", "publishToMavenLocal") }
+  .matching {
+    it.name.startsWith(PublishingPlugin.PUBLISH_LIFECYCLE_TASK_NAME)
+      && it.group == PublishingPlugin.PUBLISH_TASK_GROUP
+  }
   .configureEach {
     doLast {
       logger.lifecycle("[${this.name}] ${project.group}:${project.name}:${project.version}")
