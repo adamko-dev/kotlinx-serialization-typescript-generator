3a2af21839210c7266d42df6894e391981b08f48
Adam <897017+aSemy@users.noreply.github.com>
2022-06-02 20:15:12 +0200

    rm unused maven pom config

--- buildSrc/src/main/kotlin/buildsrc/config/publishing.kt
+++ buildSrc/src/main/kotlin/buildsrc/config/publishing.kt
@@ -2,29 +2,9 @@ package buildsrc.config
 
 import org.gradle.api.Project
 import org.gradle.api.publish.PublishingExtension
-import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.getByType
 import org.gradle.plugins.signing.SigningExtension
-
-
-fun MavenPublication.kxsTsGenPom() = pom {
-  url.set("https://github.com/adamko-dev/kotlinx-serialization-typescript-generator")
-  packaging = "jar"
-  licenses {
-    license {
-      name.set("The Apache License, Version 2.0")
-      url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-    }
-  }
-  scm {
-    connection.set("scm:git:git://github.com/adamko-dev/kotlinx-serialization-typescript-generator.git")
-    url.set("https://github.com/adamko-dev/kotlinx-serialization-typescript-generator")
-  }
-}
-
-
-
 
 
 // hacks because IntelliJ still doesn't properly load DSL accessors for buildSrc
