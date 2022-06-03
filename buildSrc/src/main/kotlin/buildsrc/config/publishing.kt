f9b48484b7d7a08c354fe49c5fd591885c1477f6
Adam <897017+aSemy@users.noreply.github.com>
2022-06-03 16:06:55 +0200

    old code clean up, and use Gradle lazy task config for KotlinCompile config

--- buildSrc/src/main/kotlin/buildsrc/config/publishing.kt
+++ buildSrc/src/main/kotlin/buildsrc/config/publishing.kt
@@ -9,16 +9,20 @@ import org.gradle.plugins.signing.SigningExtension
 
 // hacks because IntelliJ still doesn't properly load DSL accessors for buildSrc
 
+
 /** Configure [PublishingExtension] */
 fun Project.publishing(action: PublishingExtension.() -> Unit): Unit =
   extensions.configure(action)
 
+
 val Project.publishing: PublishingExtension
   get() = extensions.getByType<PublishingExtension>()
+
 
 /** Configure [SigningExtension] */
 fun Project.signing(action: SigningExtension.() -> Unit): Unit =
   extensions.configure(action)
+
 
 val Project.signing: SigningExtension
   get() = extensions.getByType<SigningExtension>()
