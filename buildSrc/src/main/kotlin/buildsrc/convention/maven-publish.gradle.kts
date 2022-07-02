36a39c69c4cf04faefd26e3d8aed957688f7dbca
Adam <897017+aSemy@users.noreply.github.com>
2022-07-02 15:18:12 +0200

    try making maven Publish task run after Sign tasks?

--- buildSrc/src/main/kotlin/buildsrc/convention/maven-publish.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/maven-publish.gradle.kts
@@ -41,6 +41,7 @@ tasks.withType<AbstractPublishToMaven>().configureEach {
   // Gradle warns about some signing tasks using publishing task outputs without explicit
   // dependencies. I'm not going to go through them all and fix them, so here's a quick fix.
   dependsOn(tasks.withType<Sign>())
+  mustRunAfter(tasks.withType<Sign>())
 
   doLast {
     logger.lifecycle("[${this.name}] ${project.group}:${project.name}:${project.version}")
