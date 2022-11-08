672047cd22a26c402816c173e4f5a0978205042c
Nathan Schulzke <nschulzke@gmail.com>
2022-11-08 10:51:23 -0700

    Add explanatory jdoc comment

--- modules/kxs-ts-gen-core/src/commonMain/kotlin/dev/adamko/kxstsgen/KxsTsAnnotations.kt
+++ modules/kxs-ts-gen-core/src/commonMain/kotlin/dev/adamko/kxstsgen/KxsTsAnnotations.kt
@@ -3,6 +3,15 @@ package dev.adamko.kxstsgen
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.InheritableSerialInfo
 
+/**
+ * Marks a property as required in the generated TypeScript interface.
+ *
+ * This annotation is inheritable, so it should be sufficient to place it on a base class of hierarchy.
+ *
+ * This annotation should only be used if [kotlinx.serialization.json.JsonConfiguration.encodeDefaults]
+ * is set to true. If it is false (which it is by default), then properties with default values are
+ * potentially omitted from the generated JSON.
+ */
 @InheritableSerialInfo
 @Target(AnnotationTarget.PROPERTY)
 @ExperimentalSerializationApi
