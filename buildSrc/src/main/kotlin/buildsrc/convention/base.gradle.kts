0ac3173fa61da48e5adb88e6c74b10f3d3593c59
Adam <897017+aSemy@users.noreply.github.com>
2022-12-31 16:39:36 +0100

    reduce/amend log output in GHA and tests

--- buildSrc/src/main/kotlin/buildsrc/convention/base.gradle.kts
+++ buildSrc/src/main/kotlin/buildsrc/convention/base.gradle.kts
@@ -1,6 +1,8 @@
 package buildsrc.convention
 
 import buildsrc.config.KxsTsGenBuildSettings
+import java.time.Duration
+import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
   base
@@ -11,5 +13,25 @@ if (project != rootProject) {
   project.version = rootProject.version
 }
 
-
 extensions.create<KxsTsGenBuildSettings>(KxsTsGenBuildSettings.NAME)
+
+tasks.withType<Test>().configureEach {
+  timeout.set(Duration.ofMinutes(10))
+
+  testLogging {
+    // don't log console output - it's too noisy
+    showCauses = false
+    showExceptions = false
+    showStackTraces = false
+    showStandardStreams = false
+    events(
+      // only log test outcomes
+      TestLogEvent.PASSED,
+      TestLogEvent.FAILED,
+      TestLogEvent.SKIPPED,
+      // TestLogEvent.STARTED,
+      // TestLogEvent.STANDARD_ERROR,
+      // TestLogEvent.STANDARD_OUT,
+    )
+  }
+}
